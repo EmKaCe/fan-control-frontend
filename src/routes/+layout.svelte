765cf2ffa864bfc3179d44171027e2bb5c49cfaf
Emre Çetin <20030146+EmKaCe@users.noreply.github.com>
2023-06-06 14:22:26 +0200

    Basic Layout complete

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,52 @@
 <script>
 	import '../app.postcss';
-	import { Footer, FooterCopyright, FooterIcon, Tooltip } from 'flowbite-svelte';
-	import { CircleStack, CodeBracket } from 'svelte-heros-v2';
+	import {
+		Button,
+		ButtonGroup,
+		DarkMode,
+		Footer,
+		FooterCopyright,
+		FooterIcon,
+		NavBrand,
+		Navbar,
+		Tooltip
+	} from 'flowbite-svelte';
+	import { CircleStack, CodeBracket, Cog, Moon, Sun } from 'svelte-heros-v2';
+	const navbarClass = 'bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-200 border-gray-100 dark:border-gray-700 px-4 sm:px-10 py-2.5 w-full';
+	const navButtonClass = 'text-center font-medium focus:ring-2 focus:z-10 size inline-flex items-center justify-center px-4 py-2 text-sm text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 dark:bg-gray-800 dark:text-gray-400 hover:text-primary-700 focus:text-primary-700 dark:focus:text-white dark:hover:text-white dark:bg-gray-700 dark:text-white dark:border-gray-700 dark:hover:border-gray-600 dark:hover:bg-gray-600 focus:ring-gray-200 dark:focus:ring-gray-700 border-l-0 first:border-l first:rounded-l-lg last:rounded-r-lg';
+	const footerClass = 'bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-200 border-gray-100 dark:border-gray-700';
 </script>
 
-<div class="app">
-	<main>
+<div class="app flex flex-col h-screen">
+	<header class="drop-shadow">
+		<Navbar class={navbarClass}>
+			<NavBrand>
+				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+					>IoT Dashboard</span
+				>
+			</NavBrand>
+			<div class="flex md:order-2">
+				<ButtonGroup>
+					<DarkMode btnClass={navButtonClass}>
+						<span class="flex items-center" slot="lightIcon">
+							<Sun /><span class="hidden md:block">Helles Design</span>
+						</span>
+						<span class="flex items-center" slot="darkIcon">
+							<Moon /><span class="hidden md:block">Dunkeles Design</span>
+						</span>
+					</DarkMode>
+					<Button>
+						<Cog />
+						<span class="hidden md:block">Einstellungen</span>
+					</Button>
+				</ButtonGroup>
+			</div>
+		</Navbar>
+	</header>
+	<main class="flex-grow container mx-auto p-10 md:px-12">
 		<slot />
 	</main>
-
-	<Footer footerType="socialmedia">
+	<Footer class={footerClass} footerType="socialmedia">
 		<div class="md:flex md:justify-between">
 			<FooterCopyright by="Leo Tietz & Emre Cetin" year={2023} />
 			<div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
@@ -19,14 +56,14 @@
 				>
 					<CodeBracket />
 				</FooterIcon>
-				<Tooltip>Front-End on GitHub</Tooltip>
+				<Tooltip>Front-End auf GitHub</Tooltip>
 				<FooterIcon
 					href="https://github.com/U1F984/fan-control-backend"
 					class="text-gray-400 hover:text-gray-900"
 				>
 					<CircleStack />
 				</FooterIcon>
-				<Tooltip>Backend on GitHub</Tooltip>
+				<Tooltip>Backend auf GitHub</Tooltip>
 			</div>
 		</div>
 	</Footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,28 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
+	import { Card } from 'flowbite-svelte';
 </script>
 
 <svelte:head>
 	<title>IoT Dashboard</title>
 	<meta name="description" content="IoT Dashboard" />
+	<script>
+		if (
+			localStorage.getItem('color-theme') === 'dark' ||
+			(!('color-theme' in localStorage) &&
+				window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	</script>
 </svelte:head>
 
-<section>
-	<div class="p-8">
-		<Alert>
-			<span class="font-medium">Info alert!</span> Hello World!
-		</Alert>
-	</div>
+<section class="grid md:grid-cols-2 md:grid-rows-2 gap-4 place-items-center h-full">
+	{#each [0, 1, 2, 3] as e}
+		<Card class="w-full">
+			<h5>Test</h5>
+			<p>test card</p>
+		</Card>
+	{/each}
 </section>
