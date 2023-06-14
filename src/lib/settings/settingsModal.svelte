e9685a0f5d5c6c68f47babf162d8c5d80473361b
Emre Çetin <20030146+EmKaCe@users.noreply.github.com>
2023-06-14 18:37:41 +0200

    Inital modal

---
+++ src/lib/settings/settingsModal.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Button, Modal } from 'flowbite-svelte';
+	export let open: boolean;
+</script>
+
+<Modal
+	title="Einstellungen"
+	autoclose
+	outsideclose
+    bind:open={open}
+	on:close={() => {
+		open = false;
+	}}
+>
+	<svelte:fragment slot="footer">
+		<Button>Speichern</Button>
+		<Button color="alternative">Abbrechen</Button>
+	</svelte:fragment>
+</Modal>
