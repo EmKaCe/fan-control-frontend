5673eb16cf8fcc6717760a847117d325f1673ac2
Emre Çetin <20030146+EmKaCe@users.noreply.github.com>
2023-06-16 16:26:31 +0200

    Settings Modal with tooltips

---
+++ src/lib/settings/tooltipLabel.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+	import { Button, Label, Tooltip } from "flowbite-svelte";
+	import { QuestionMarkCircle } from "svelte-heros-v2";
+
+    export let forId: string;
+    export let title: string;
+    export let tooltip: string;
+</script>
+
+<Label for={forId} class="flex flex-row justify-between mb-2">
+    {title}
+    <Button btnClass="p-0" outline={true} pill={true}>
+        <QuestionMarkCircle class="text-primary-600 h-6 w-6" />
+    </Button>
+    <Tooltip class="w-72">
+        {tooltip}
+    </Tooltip>
+</Label>
